cfa217daec25bf1c105ef502522d72ece4ee323a
TANIGUCHI Takaki <takaki@asis.media-as.org>
2016-06-10 10:55:21 +0900

    install material-ui

--- entry.js
+++ entry.js
@@ -1,8 +1,15 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
 
 ReactDOM.render(
-  <h1>Hello, world</h1>,
+  <div>
+    <h1>Hello, world</h1>
+    <MuiThemeProvider>
+      <RaisedButton label="Default" />
+    </MuiThemeProvider>
+  </div>,
   document.getElementById('content')
 );
