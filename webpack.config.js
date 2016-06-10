12eb047514d662372bff3412accbc6bc5f54febd
TANIGUCHI Takaki <takaki@asis.media-as.org>
2016-06-10 13:17:06 +0900

    exlucde node files

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js$/,  loader: "babel-loader" }
+      { test: /\.js$/,  loader: "babel-loader" , exclude: /node_modules/ }
     ]
   }
 }
