10ca4b93156082112759edd9d769bfa6cf12d671
TANIGUCHI Takaki <takaki@asis.media-as.org>
2016-06-10 10:49:42 +0900

    init

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  entry: "./entry.js",
+  output: {
+    path: __dirname,
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/,  loader: "babel-loader" }
+    ]
+  }
+}
